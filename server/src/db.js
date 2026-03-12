d0ad62fdd6c2135fd51883c86fbc76906c4acf09
JeevakumaranS <jeevas0028@gmail.com>
2026-03-12 23:53:49 +0530

    db updates

--- server/src/db.js
+++ server/src/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise");
 const bcrypt = require("bcryptjs");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "onspot",
+  host: process.env.mysql.railway.internal,
+  port: Number(process.env.MYSQLPORT || 3306),
+  user: process.env.root,
+  password: process.env.xNHUECKNXUZovIBkyWQodAEbKPqCyyxu,
+  database: process.env.railway,
   waitForConnections: true,
   connectionLimit: 10,
   namedPlaceholders: true,
